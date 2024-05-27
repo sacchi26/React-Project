8d953b1e50290ed22f59cbcdd68c9895127b858b
Sacchida Khandelwal <khandelwalkhushi858@gmail.com>
2024-05-27 11:13:35 +0530

    added

--- Frontent/final-react/src/main.jsx
+++ Frontent/final-react/src/main.jsx
@@ -7,11 +7,11 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContextProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthContextProvider>
     <BrowserRouter>
       <ChakraProvider>
+        <AuthContextProvider>
         <App />
+        </AuthContextProvider>
       </ChakraProvider>
     </BrowserRouter>
-  </AuthContextProvider>
 );
